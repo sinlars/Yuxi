d994522e21bf15ecc25bfe179cda1e14e588c967
sinlars <sinlars@163.com>
2026-06-05 15:08:30 +0800

    增加开发环境dev.test.com的访问权限

--- web/vite.config.js
+++ web/vite.config.js
@@ -24,6 +24,7 @@ export default defineConfig(({ mode }) => {
         ignored: ['**/node_modules/**', '**/dist/**'],
       },
       host: '0.0.0.0',
+      allowedHosts: ['dev.test.com'],
     }
   }
 })
